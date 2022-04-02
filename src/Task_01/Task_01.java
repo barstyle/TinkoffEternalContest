734e53f74f8d5963ad58a6ed3f5e3dd7a4d518f0
Georgiy Goloborodov <barstyle@mail.ru>
2022-04-02 19:05:09 +0300

    add first task

---
+++ src/Task_01/Task_01.java
@@ -0,0 +1,23 @@
+package Task_01;
+
+import java.util.Scanner;
+
+public class Task_01 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int fareCost = scanner.nextInt(); // стоимость тарифа
+        int fareAmount = scanner.nextInt(); // размер тарифа
+        int overPriceMb = scanner.nextInt(); // стоимость каждого лишнего мегабайта
+        int trafficSize = scanner.nextInt(); // размер интернет рафика в следующем месяце
+
+        int result;
+
+        if (trafficSize > fareAmount) {
+            result = (overPriceMb * (trafficSize - fareAmount)) + fareCost;
+        } else result = fareCost;
+
+        System.out.println(result);
+    }
+}
